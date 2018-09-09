f407404b58a728e45c440fbb30f87aeaabbcde3e
Ruben <rweel@users.noreply.github.com>
2018-09-09 16:55:20 +0200

    Add files via upload

---
+++ Big Belly assets.py
@@ -0,0 +1,14 @@
+"""
+This script makes a list of the assets of a city with Big Belly waste containers
+This script is whritten in the API doc, more info https://clean.bigbelly.com
+You need a token for this script
+"""
+# make sure you install the requests package
+import requests
+# the url of the API
+url = "https://api.bigbelly.com/api/v2?objectType=assets&action=load"
+querystring = {"objectType":"accounts","action":"load"}
+# you need a token at // token here //
+headers = {'X-Token': "// token here //", 'Cache-Control': "no-cache"}
+response = requests.request("GET", url, headers=headers, params=querystring)
+print(response.text)
